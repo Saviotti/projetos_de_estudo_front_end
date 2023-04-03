a397cbad96fe900f018efacdf1fe98f30abcf14b
Seu nome <marcusaviotti@gmail.com>
2023-04-03 17:15:31 -0300

    Feat Desenvolvido configurações inciais e página de Login com os testes

---
+++ src/pages/Login.js
@@ -0,0 +1,66 @@
+import React, { useState, useContext, useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Context } from '../context/index';
+import { validateLogin } from '../utils/validateLogin';
+
+const INITIAL_STATE = {
+  email: '',
+  btDisabled: true,
+  password: '',
+};
+
+export default function Login() {
+  const [state, setState] = useState(INITIAL_STATE);
+  const { setEmail } = useContext(Context);
+
+  const handleInputChange = useCallback(({ target }) => {
+    setState((prevState) => ({
+      ...prevState, [target.name]: target.value, btDisabled: validateLogin(prevState),
+    }));
+  }, []);
+
+  const history = useHistory();
+
+  const handleClick = () => {
+    const { email } = state;
+    localStorage.setItem('user', JSON.stringify({ email }));
+    setEmail(email);
+    history.push('/meals');
+  };
+
+  return (
+    <div>
+      <h1 className="h1">LOGIN</h1>
+      <div className="login">
+        <input
+          data-testid="email-input"
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={ state.email }
+          onChange={ handleInputChange }
+          id="email-input"
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={ state.password }
+          onChange={ handleInputChange }
+          id="password-input"
+        />
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+          name="login-submit-btn"
+          id="login-submit-btn"
+          disabled={ state.btDisabled }
+          onClick={ handleClick }
+        >
+          Enter
+        </button>
+      </div>
+    </div>
+  );
+}

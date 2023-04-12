cd4e248f788c3cf4dd2a34800fa8c1bb443a0411
Aline <policarpo_aline@hotmail.com>
2023-04-11 22:33:15 -0300

    lint

--- src/tests/FavoriteRecipes.test.js
+++ src/tests/FavoriteRecipes.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, act, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import FavoriteRecipes from '../pages/FavoriteRecipes';
 import renderWithRouter from '../services/renderWithRouter';
